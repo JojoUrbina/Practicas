c369d1e15b3cca801d7df7e84fb682b4fdefc0b2
JojoUrbina <95663502+JojoUrbina@users.noreply.github.com>
2022-02-14 21:53:39 -0300

    actualizacion
    haciendo pruebas con github Desktop

--- Tarea 1/tarea1.js
+++
@@ -1,8 +0,0 @@
-const $botonCalcular =document.querySelector('#calcular')
-$botonCalcular.onclick = function(){
-    const mesesAlAño=12;
-    const salarioAnual = Number(document.querySelector('#salario-anual-usuario').value);
-    document.querySelector('#salario-mensual').value=Number(salarioAnual/mesesAlAño)
-    ;
-}
-
